4080bf8781e3eb7f7f8ed1e7aeb196833165a7c6
Yuriy Stul <Yuriy.Stul@WilliamHill.com>
2017-10-30 17:14:02 +0200

    create actors

--- akka/data-watcher1/src/main/scala/com/stulsoft/akka/data/watcher1/actor/DataWatcherSupervisorActor.scala
+++ akka/data-watcher1/src/main/scala/com/stulsoft/akka/data/watcher1/actor/DataWatcherSupervisorActor.scala
@@ -4,7 +4,10 @@
 
 package com.stulsoft.akka.data.watcher1.actor
 
-import akka.actor.{Actor, ActorLogging}
+import akka.actor.SupervisorStrategy.Stop
+import akka.actor.{Actor, ActorLogging, OneForOneStrategy, Props, SupervisorStrategy}
+import com.stulsoft.akka.data.watcher1.Exceptions.DiskErrorException
+import com.stulsoft.akka.data.watcher1.actor.DataWatcherSupervisorActor.StartDataWatcher
 
 /**
   * Data watcher supervisor actor
@@ -12,5 +15,24 @@ import akka.actor.{Actor, ActorLogging}
   * @author Yuriy Stul
   */
 class DataWatcherSupervisorActor extends Actor with ActorLogging {
-  override def receive: Receive = ???
+
+  override def supervisorStrategy: SupervisorStrategy = OneForOneStrategy() {
+    case _: DiskErrorException => Stop
+  }
+
+  override def preStart(): Unit ={
+    log.info("Started DataWatcherSupervisorActor")
+    super.preStart()
+  }
+
+  override def receive: Receive = {
+    case StartDataWatcher(path) =>
+      context.actorOf(Props[DataWatcherActor]) // todo add path
+  }
+}
+
+object DataWatcherSupervisorActor {
+
+  case class StartDataWatcher(path: String)
+
 }
--- akka/data-watcher1/src/main/scala/com/stulsoft/akka/data/watcher1/actor/FileProcessorActor.scala
+++ akka/data-watcher1/src/main/scala/com/stulsoft/akka/data/watcher1/actor/FileProcessorActor.scala
@@ -4,12 +4,24 @@
 
 package com.stulsoft.akka.data.watcher1.actor
 
-import akka.actor.{Actor, ActorLogging}
+import akka.actor.SupervisorStrategy.Restart
+import akka.actor.{Actor, ActorLogging, OneForOneStrategy, SupervisorStrategy}
+import com.stulsoft.akka.data.watcher1.Exceptions.DBConnectionException
 
 /** File processor actor
   *
   * @author Yuriy Stul
   */
 class FileProcessorActor extends Actor with ActorLogging {
+
+  override def preStart(): Unit = {
+    log.info("Started FileProcessorActor")
+    super.preStart()
+  }
+
+  override def supervisorStrategy: SupervisorStrategy = OneForOneStrategy() {
+    case _: DBConnectionException => Restart
+  }
+
   override def receive: Receive = ???
 }
