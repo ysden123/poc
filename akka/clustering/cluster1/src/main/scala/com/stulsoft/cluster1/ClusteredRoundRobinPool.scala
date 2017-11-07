4a5ceaa8886a47c917eea7ce3c24a294ddc759bf
Yuriy Stul <Yuriy.Stul@WilliamHill.com>
2017-11-07 11:35:19 +0200

    logging

--- akka/clustering/cluster1/src/main/scala/com/stulsoft/cluster1/ClusteredRoundRobinPool.scala
+++ akka/clustering/cluster1/src/main/scala/com/stulsoft/cluster1/ClusteredRoundRobinPool.scala
@@ -4,6 +4,7 @@ import akka.actor.{Actor, ActorLogging, ActorSystem, Props}
 import akka.cluster.Cluster
 import akka.routing.FromConfig
 import com.typesafe.config.ConfigFactory
+import com.typesafe.scalalogging.LazyLogging
 
 import scala.concurrent.{Await, Future}
 
@@ -11,7 +12,7 @@ import scala.concurrent.{Await, Future}
   * @see [[https://gist.github.com/johanandren/d2b874e59320e07f84a4 Simple sample with clustered round robin pool]]
   * @author Yuriy Stul.
   */
-object ClusteredRoundRobinPool extends App {
+object ClusteredRoundRobinPool extends App with LazyLogging {
 
   val commonConfig = ConfigFactory.parseString(
     """
@@ -43,6 +44,7 @@ object ClusteredRoundRobinPool extends App {
       |}
     """.stripMargin)
 
+  logger.info("Starting actor systems...")
   implicit val system1: ActorSystem = ActorSystem("cluster", ConfigFactory.parseString(
     """
       |akka.remote.netty.tcp.port = 2551
@@ -58,13 +60,16 @@ object ClusteredRoundRobinPool extends App {
       |akka.remote.netty.tcp.port = 2553
     """.stripMargin).withFallback(commonConfig))
 
-
+  logger.info("Waiting 3 members...")
   while (Cluster(system1).state.members.size != 3) {
     Thread.sleep(250)
   }
 
   class EchoActor extends Actor with ActorLogging {
-    log.info("Starting")
+    override def preStart(): Unit = {
+      super.preStart()
+      log.info("Starting EchoActor with {} name", self.path.name)
+    }
 
     override def receive: Receive = {
       case "ping" =>
@@ -73,21 +78,19 @@ object ClusteredRoundRobinPool extends App {
     }
   }
 
+  logger.info("Creating pool of actors")
   val pool = system1.actorOf(FromConfig.props(Props[EchoActor]), "echo")
 
 
   import scala.concurrent.ExecutionContext.Implicits.global
   import scala.concurrent.duration._
 
+  logger.info("Sending message each 1 second")
   system1.scheduler.schedule(1.second, 1.second, pool, "ping")
 
-  /*
-    println("Enter to quit")
-    StdIn.readLine()
-  */
-
+  logger.info("Wait 5 seconds")
   Thread.sleep(1000 * 5)
 
+  logger.info("Terminating all actor systems...")
   Await.result(Future.traverse(List(system1, system2, system3))(system => system.terminate()), Duration.Inf)
-
 }
