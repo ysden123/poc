f1063d981162526ddc0bf405fc1690950cd075cc
Yuriy Stul <Yuriy.Stul@WilliamHill.com>
2017-11-28 12:49:53 +0200

    ShowDirsandFile

---
+++ FS/src/FS.java
@@ -0,0 +1,67 @@
+public class FS {
+    private static final String COMMAND_ADD_DIR = "addDir";
+    private static final String COMMAND_DELETE_DIR = "deleteDir";
+    private static final String COMMAND_ADD_FILE = "addFile";
+    private static final String COMMAND_DELETE_FILE = "deleteFile";
+    private static final String COMMAND_SHOW_DIRSAND_FILE = "show";
+
+    private Node tree = new Node("root", 0L, null, 0);
+
+    private void AddDir(String name, String color, long id, long parent_id) {
+        Node newDir = new Node(name, id, color, parent_id);
+        tree.addNode(newDir);
+    }
+
+    private void DeleteDir(long id) {
+        tree.deleteNode(id);
+    }
+
+    private void AddFile(String name, long id, String color, long parent_id) {
+        Node newFile = new Node(name, id, color, parent_id);
+        tree.addNode(newFile);
+    }
+
+    private void DeleteFile(long id) {
+        tree.deleteNode(id);
+    }
+
+    private void ShowDirsandFile() {
+        Node nodes[] = tree.getAllNodes();
+        for (int i = 0; i < nodes.length; ++i) {
+            if (nodes[i] == null)
+                break;
+            System.out.println(nodes[i]);
+        }
+    }
+
+    /**
+     * Command: command, id,color,parent_id
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+//        System.out.println(args[0]);
+        FS fs = new FS();
+/*
+        switch (args[0]) {
+            case COMMAND_ADD_DIR:
+                fs.AddDir(args[1], args[2], Long.parseLong(args[3]), Long.parseLong(args[4]));
+                break;
+            case COMMAND_DELETE_DIR:
+                fs.DeleteDir(Long.parseLong(args[1]));
+                break;
+            case COMMAND_ADD_FILE:
+                fs.AddFile(args[1], Long.parseLong(args[2]), args[3], Long.parseLong(args[4]));
+                break;
+            case COMMAND_DELETE_FILE:
+                fs.DeleteFile(Long.parseLong(args[1]));
+                break;
+            case COMMAND_SHOW_DIRSAND_FILE:
+                fs.ShowDirsandFile();
+        }
+*/
+        fs.AddDir("test1", "red", 1, 0);
+        fs.AddFile("file1", 2, "blue", 1);
+        fs.ShowDirsandFile();
+    }
+}
---
+++ FS/src/Node.java
@@ -0,0 +1,132 @@
+import java.util.Arrays;
+
+public class Node {
+    private final String name;
+    private final long id;
+    private final String color;
+    private final Node children[];
+    private final long parent_id;
+    private int childSize;
+
+    public Node(String name, long id, String color, long parent_id) {
+        this.name = name;
+        this.id = id;
+        this.color = color;
+        this.parent_id=parent_id;
+        children = new Node[255];
+        childSize = 0;
+    }
+
+    public void addNode(Node node){
+        // find parent
+        Node parent = findNode(this, node.getParent_id());
+        if (parent == null)
+            addNodeToParent(this, node);
+        else
+            addNodeToParent(parent,node);
+    }
+
+    public void addNodeToParent(Node parent, Node node){
+        parent.children[parent.getChildSize()] = node;
+        parent.setChildSize(parent.getChildSize() + 1);
+    }
+
+    public void deleteNode(long id){
+        boolean startDelete = false;
+        for(int i=0; i < childSize - 1;++i){
+            if (!startDelete){
+                if (children[i].getId() == id){
+                    startDelete = true;
+                }
+            }
+            if (startDelete){
+                children[i] = children[i + 1];
+            }
+        }
+        childSize--;
+    }
+
+    /**
+     * Finds node with specified id inside parent Node
+     * @param parent
+     * @param id
+     * @return
+     */
+    private Node findNode(Node parent, long id){
+        // check, if parent has node with id
+        for(int i=0; i < parent.getChildSize();++i){
+            if (children[i].getId()== id){
+                return children[i];
+            }
+        }
+
+        // if now call findNode for all children
+        Node foundNode = null;
+        for(int i=0;i<parent.getChildSize();++i){
+            if (children[i].getChildSize() > 0){
+                foundNode = findNode(children[i],id);
+            }
+            if (foundNode!=null){
+                return foundNode;
+            }
+        }
+
+        return null;
+    }
+
+    public Node[] getAllNodes(){
+        Node nodes[] = new Node[255];
+        int pointer = 0;
+        for(int i=0; i < getChildSize();++i){
+            nodes[pointer++] = children[i];
+            if (children[i].getChildSize() > 0){
+                Node receivedNodes[] = children[i].getAllNodes();
+                for(Node node:receivedNodes){
+                    if (node == null)
+                        break;
+                    nodes[pointer++] = node;
+                }
+            }
+        }
+        return nodes;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public Node[] getChilds() {
+        return children;
+    }
+
+    public long getParent_id() {
+        return parent_id;
+    }
+
+    public int getChildSize() {
+        return childSize;
+    }
+
+    @Override
+    public String toString() {
+        return "Node{" +
+                "name='" + name + '\'' +
+                ", id=" + id +
+                ", color='" + color + '\'' +
+                ", parent_id=" + parent_id +
+                ", childSize=" + childSize +
+                '}';
+    }
+
+    public void setChildSize(int childSize) {
+        this.childSize = childSize;
+    }
+}
