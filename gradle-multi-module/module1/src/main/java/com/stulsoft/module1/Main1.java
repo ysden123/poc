0814566f4acb16e5e2bfe43f199956bd07008213
Yuriy Stul <Yuriy.Stul@WilliamHill.com>
2018-01-07 10:31:49 +0200

    Multi-module Java project on Gradle

---
+++ gradle-multi-module/module1/src/main/java/com/stulsoft/module1/Main1.java
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2018, William Hill Online. All rights reserved
+ */
+package com.stulsoft.module1;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author Yuriy Stul.
+ */
+public class Main1 {
+    private static Logger logger = LoggerFactory.getLogger(Main1.class);
+    public static void main(String[] args) {
+        logger.info("==>main");
+        logger.info("<==main");
+    }
+}
