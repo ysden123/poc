e7ac47b9bdd356815b73299b033837494a830806
Yuriy Stul <yuriy.s@dauup-clicksmob.com>
2018-02-06 16:38:39 +0200

    Main & tests

---
+++ pspring-scala/src/main/scala/com/stulsoft/pspring/scala/Main1.scala
@@ -0,0 +1,20 @@
+/*
+   Created by Yuriy Stul 2018
+*/
+package com.stulsoft.pspring.scala
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.{CommandLineRunner, SpringApplication}
+
+@SpringBootApplication
+class Main1 extends CommandLineRunner {
+  override def run(strings: String*): Unit = {
+    println(s"==>Main1.run")
+  }
+}
+
+object Main1 {
+  def main(args: Array[String]): Unit = {
+    SpringApplication.run(classOf[Main1], args: _*)
+  }
+}
