d2084af9dd4e1c9bff8245e9d4e167df0d717a26
Yuriy Stul <yuriy.s@dauup-clicksmob.com>
2018-02-13 15:10:56 +0200

    add with array

--- gcp/datastore-test/src/main/java/com/stulsoft/gcp/datastore/Main1.java
+++ gcp/datastore-test/src/main/java/com/stulsoft/gcp/datastore/Main1.java
@@ -5,49 +5,13 @@ package com.stulsoft.gcp.datastore;
 
 import com.google.cloud.datastore.*;
 
+import static com.stulsoft.gcp.datastore.Utils.addManyValues;
+
 /**
  * @author Yuriy Stul
  * @see <a href="https://cloud.google.com/datastore/docs/reference/libraries#client-libraries-usage-java">Cloud Datastore Client Libraries</a>
  */
 public class Main1 {
-
-    private static void addManyValues(Datastore datastore, int n) {
-        System.out.println("==>addManyValues");
-        try {
-            Duration totalDuration = new Duration();
-            // The kind for the new entity
-            String kind = "Task";
-            Duration factoryDuration = new Duration();
-            KeyFactory keyFactory = datastore.newKeyFactory();
-            keyFactory.setKind(kind);
-            System.out.printf("Factory creation duration is %d ms.%n", factoryDuration.duration());
-
-            Duration duration = new Duration();
-            long durationSum = 0L;
-            for (int i = 1; i <= n; ++i) {
-                duration.start();
-                // The name/ID for the new entity
-                String name = String.format("sampletask %d", i);
-                // The Cloud Datastore key for the new entity
-                Key taskKey = keyFactory.newKey(name);
-
-                // Prepares the new entity
-                Entity task = Entity.newBuilder(taskKey)
-                        .set("description", String.format("Buy milk %d", i))
-                        .build();
-
-                // Saves the entity
-                datastore.put(task);
-                durationSum += duration.duration();
-            }
-            System.out.printf("Added %d entities during %d ms.%n", n, totalDuration.duration());
-            System.out.printf("durationSum = %d ms. Average duration for one entity is %d ms.%n", durationSum,
-                    durationSum / n);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        System.out.println("<==addManyValues");
-    }
 
     public static void main(String... args) {
         try {
