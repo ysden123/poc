4a568bcd8d659c5a7105ab0e895da430d51b2b89
Yuriy Stul <yuriy.s@dauup-clicksmob.com>
2018-02-28 16:35:55 +0200

    Unit testing with JUnit4

---
+++ pjunit4/src/main/java/com/stulsoft/pjunit4/SomeClass.java
@@ -0,0 +1,14 @@
+/*
+   Created by Yuriy Stul 2018
+*/
+package com.stulsoft.pjunit4;
+
+/**
+ * @author Yuriy Stul
+ * @since 2/28/2018
+ */
+public class SomeClass {
+    public int foo(String text) {
+        return text.length();
+    }
+}
