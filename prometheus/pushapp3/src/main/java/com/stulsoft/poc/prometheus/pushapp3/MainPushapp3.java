80d33293b09194271fa1696a81df584fc6f56d1f
yuriy <yuriy.s@dauup-clicksmob.com>
2018-05-23 10:04:02 +0300

    Initial project Pushapp3

---
+++ prometheus/pushapp3/src/main/java/com/stulsoft/poc/prometheus/pushapp3/MainPushapp3.java
@@ -0,0 +1,22 @@
+/*
+ * Created by Yuriy Stul 23 May 2018
+ */
+package com.stulsoft.poc.prometheus.pushapp3;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author Yuriy Stul
+ *
+ */
+public class MainPushapp3 {
+private static final Logger logger = LoggerFactory.getLogger(MainPushapp3.class);
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		logger.info("Started MainPushapp3");
+		logger.info("Stoped MainPushapp3");
+	}
+}
