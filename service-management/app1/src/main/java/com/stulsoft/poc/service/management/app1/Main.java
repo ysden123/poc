ba6aa59730ce187edf4aa3abcc1a847c63b00934
yuriy <yuriy.s@dauup-clicksmob.com>
2018-05-28 09:26:22 +0300

    initial project

---
+++ service-management/app1/src/main/java/com/stulsoft/poc/service/management/app1/Main.java
@@ -0,0 +1,24 @@
+/*
+ * Created by Yuriy Stul 28 May 2018
+ */
+package com.stulsoft.poc.service.management.app1;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author Yuriy Stul
+ *
+ */
+public class Main {
+	private static final Logger logger = LoggerFactory.getLogger(Main.class);
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		logger.info("Started app1");
+
+	}
+
+}
