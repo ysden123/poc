df6a38b5cc4d47a6785d7569d2887639ff64c76f
Yuriy Stul <ysden123@gmail.com>
2018-08-11 18:29:40 +0300

    hard-coded version

---
+++ pom-experiments/pomtest1/common/src/main/java/com/stulsoft/pom/experiments/common/util/Utils.java
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2018. Yuriy Stul
+ */
+
+package com.stulsoft.pom.experiments.common.util;
+
+/**
+ * @author Yuriy Stul
+ */
+public class Utils {
+    private Utils() {
+    }
+
+    static String testText() {
+        return "Some test text";
+    }
+
+    public static void main(String[] args){
+System.out.println(testText());
+    }
+}
