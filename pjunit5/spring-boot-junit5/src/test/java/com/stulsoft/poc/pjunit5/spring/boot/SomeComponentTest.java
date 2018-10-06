fea0036094a6b653ad20535b0a6c071de022aab2
Yuriy Stul <ysden123@gmail.com>
2018-10-06 16:54:11 +0300

    Unit tests

---
+++ pjunit5/spring-boot-junit5/src/test/java/com/stulsoft/poc/pjunit5/spring/boot/SomeComponentTest.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) 2018. Yuriy Stul
+ */
+
+package com.stulsoft.poc.pjunit5.spring.boot;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+/**
+ * @author Yuriy Stul
+ */
+@ExtendWith(SpringExtension.class)
+@TestPropertySource(properties = "app.scheduling.enable=false")
+@SpringBootTest
+@ComponentScan(basePackages = "com.stulsoft.poc.pjunit5.spring.boot")
+class SomeComponentTest {
+    @Autowired
+    private SomeComponent someComponent = null;
+
+    @Test
+    void work() {
+        someComponent.work();
+    }
+}
