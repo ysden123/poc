04f3a26af64737e95e178f5404270e614cbffde3
Yuriy Stul <yuriy.s@webpals.com>
2020-09-07 16:11:51 +0300

    refactoring

--- json-path-2-attr/src/main/scala/com/stulsoft/json/Utils.scala
+++ json-path-2-attr/src/main/scala/com/stulsoft/json/Utils.scala
@@ -4,6 +4,7 @@
 
 package com.stulsoft.json
 
+import org.json4s.DefaultFormats
 import org.json4s.jackson.JsonMethods.parse
 
 import scala.io.Source
@@ -12,15 +13,14 @@ import scala.io.Source
  * @author Yuriy Stul
  */
 object Utils {
+  implicit val formats: DefaultFormats.type = org.json4s.DefaultFormats
   def jsonMapFromResource(path: String): Map[String, Any] = {
-    implicit val formats = org.json4s.DefaultFormats
     val r = Source.fromResource(path)
     val jsonStr = r.getLines().mkString
     parse(jsonStr).extract[Map[String, Any]]
   }
 
   def jsonMapFromFile(path: String): Map[String, Any] = {
-    implicit val formats = org.json4s.DefaultFormats
     val r = Source.fromFile(path)
     val jsonStr = r.getLines().mkString
     parse(jsonStr).extract[Map[String, Any]]
