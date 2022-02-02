671bcf40bf500ba6f63c6ba7025ea82f0e7389ae
Yuriy Stul <ysden123@gmail.com>
2022-02-02 22:19:13 +0200

    lombok

---
+++ plombok/app/src/main/java/com/stulsoft/poc/TestDTO.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2022, StulSoft
+ */
+
+package com.stulsoft.poc;
+
+import lombok.*;
+
+@AllArgsConstructor
+@ToString
+@Getter
+@Setter
+public class TestDTO {
+    private String name;
+    private Integer age;
+}
